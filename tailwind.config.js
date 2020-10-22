64a186eb4c2884f05c0be8322d9ade3c135c443c
Unai Mengual <20427094+owlnai@users.noreply.github.com>
2020-10-22 08:27:51 +0000

    Building

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: {
+    mode: 'all',
+   content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ]},
+  theme: {
+    extend: {
+      fontSize: {
+       100: '120px'
+},
+       fontFamily: {
+          'sans': ['Inter', 'Inter var', 'Comic Sans MS']
+       },
+      colors: {
+        tsuki: { 
+          black: '#131016',
+          lavender: '#9E9ACC',
+        }
+      },
+      spacing: {
+        7: '1.75rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
